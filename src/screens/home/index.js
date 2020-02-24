86d787e64068bf5a947e001a49882a5fa4b9cd91
Steven Baumann <steven.baumann@mymail.champlain.edu>
2020-02-24 02:23:06 -0500

    Removed warnings.

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,12 +1,8 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { createMuiTheme, CssBaseline } from '@material-ui/core';
 import BackgroundParticles from '../../components/background-particles';
 import { makeStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
 import { ThemeProvider } from '@material-ui/styles';
-import Grid from '@material-ui/core/Grid';
-import CenterSpace from '../../components/center-space';
-import Button from '@material-ui/core/Button';
 import Main from '../main';
 
 const useStyles = makeStyles({
